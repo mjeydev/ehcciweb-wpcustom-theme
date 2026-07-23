47113fd4fc23c772e871f5be1875c781cd51fd8d
mjey015 <laccay.mjerome@gmail.com>
2026-07-23 13:57:13 -0400

    Header Done

--- header.php
+++ header.php
@@ -25,7 +25,7 @@
             <a href="<?php echo esc_url(home_url('/')); ?>">
 
                 <img
-                    src="<?php echo get_template_directory_uri(); ?>/assets/images/logo.png"
+                    src="<?php echo get_template_directory_uri(); ?>/assets/images/ehccilogo.png"
                     alt="<?php bloginfo('name'); ?>">
 
             </a>
